e8f92561b1cf087099f69f52116568f82414dbb9
PauloDezingrini <62103829+PauloDezingrini@users.noreply.github.com>
2021-10-12 20:12:44 -0300

    Inclusão da classe TSPfiles visando melhorar a leitura e o processamento de dados

--- main.py
+++ main.py
@@ -1,116 +1,16 @@
 # -*- coding: utf-8 -*-
-
-from ponto import *
+from TSPfiles import TSPfile
 from solucao import *
-from math import sqrt
-
-# Remove um elemento qualquer de uma lista , é necessário já que ao quebrar a linha para obter os 3 valores(número do ponto , coord x ,coord y)
-# sobra umas strings vazias e espaços que são delatados através desta função
-def remove_values_from_list(the_list, val):
-   return [value for value in the_list if value != val]
-
-
-# Calcula a matriz de distancias e a retorna. A matriz de distancias será n x n , onde n é o numero de pontos lidos a partir do arquivo
-def calcularDistancias(lista_de_pontos):
-    matrizDeDistancias = []
-    for i in range(len(lista_de_pontos)):
-        linha = []
-        for j in range(len(lista_de_pontos)):
-            if(i==j):
-                linha.append(0)
-            else:
-                ponto1 = lista_de_pontos[i]
-                ponto2 = lista_de_pontos[j]
-                x1 = ponto1.getX()
-                y1 = ponto1.getY()
-                x2 = ponto2.getX()
-                y2 = ponto2.getY()
-                dist = sqrt((x2 - x1)**2 + (y2  - y1)**2)
-                linha.append(dist)
-        matrizDeDistancias.append(linha)
-    return matrizDeDistancias
-
-# Calcula a quantidade de elementos na matriz, usada apenas em testes.
-def contarElementosMatriz(matriz):
-    numeroElementos = 0
-    for i in range(len(matriz)):
-        for j in range(len(matriz[i])):
-            numeroElementos+=1
-    print(numeroElementos)
-
-
-def lerMatriz(arquivo_a_ser_lido):
-    file = open(arquivo_a_ser_lido,'r')
-    matriz = []
-    readDists = False
-    type = ''
-    while True:
-        str = file.readline()
-        if "EDGE_WEIGHT_FORMAT: UPPER_ROW " in str:
-            type = 'Upper_row'
-        if (str == "NODE_COORD_SECTION\n" or str =="DISPLAY_DATA_SECTION\n"):
-            break
-        if readDists:
-            aux = []
-            str = str.split(" ")
-            str = remove_values_from_list(str," ")
-            str = remove_values_from_list(str,"")
-            str[-1] = str[-1].strip('\n')
-            for i in range(len(str)):
-                aux.append(int(str[i]))
-            matriz.append(aux)
-        if("EDGE_WEIGHT_SECTION" in str):
-            readDists = True
-    if type == 'Upper_row':
-        print("HBAKLK")
-        # for i in range(len(matriz)):
-        #     matriz[i].insert(0,0)
-        size = set(range(len(matriz) + 1))
-        matriz2 = [[0 if i ==j else matriz[i][j-i-1] if j>i else matriz[j][i-j-1] for j in size] for i in size]
-        matriz = matriz2
-
-    return matriz
-
-# Responsável por fazer a leitura do arquivo , falta implementar os casos de entradas de dados especiais. Como quando recebe direto a matriz
-# de distâncias
-def realizarLeitura(arquivo_a_ser_lido):
-    # file = open('a280.txt','r')
-    file = open(arquivo_a_ser_lido,'r')
-    str = "a"
-    lista = []
-    lerCoord = False
-    while True:
-        str  = file.readline()
-        if str == "EOF" or str == "EOF\n":
-            break
-        if lerCoord:
-            # Separa a linha a partir dos espaçoes e então remove os espaços vazios que restarem devido a espaços seguidos e strings "" que 
-            # estavam sobrando no array
-            aux = str.split(" ")
-            filtered = remove_values_from_list(aux," ")
-            filtered = remove_values_from_list(filtered,"")
-            # Remove os \n do final do ultimo valor de cada linha
-            filtered[2] = filtered[2].strip('\n')
-            # Adiciona para a lista o array [numero_do_ponto,x,y]
-            newPonto = Ponto(filtered[0],filtered[1],filtered[2])
-            lista.append(newPonto)
-        # Começa a leitura a partir da linha que ele encontra um dos textos abaixo.
-        if (str == "NODE_COORD_SECTION\n" or str =="DISPLAY_DATA_SECTION\n"):
-            lerCoord = True
-
-    file.close()
-    return lista
 
 
 while True:
 
     arquivo_a_ser_lido = input("Digite o nome do arquivo que será lido: ")
 
-    lista_de_pontos = realizarLeitura(arquivo_a_ser_lido)
-    matrizDistancias = lerMatriz(arquivo_a_ser_lido)
+    file = TSPfile(arquivo_a_ser_lido)
 
-    if len(matrizDistancias) == 0:
-        matrizDistancias = calcularDistancias(lista_de_pontos)
+    lista_de_pontos = file.getList()
+    matrizDistancias = file.getMatriz()
 
     numero_de_pontos = int(input("Digite o numero de pontos que terá a solução: "))
 
