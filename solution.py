f1378c3c123385f587a42ca01bc3a43bfbad865e
Paulo Dezingrini <paulo.dezingrini@ufv.br>
2022-07-19 22:45:18 -0300

    Atualização da tabela graspRVND

--- solution.py
+++ solution.py
@@ -134,7 +134,7 @@ class Solution(object):
         self.__solucao.append(1)
         neightboors = []
         cont = 1
-        alpha = 0.15
+        alpha = 0.05
         while(cont < n_points):
             neightboors = self.getCloserNeightboors(self.__solucao[-1])
             m = max(0, floor(alpha*len(neightboors) - 1))
